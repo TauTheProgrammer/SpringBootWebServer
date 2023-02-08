7ae484838c9ec0cccf033e56752e78f8253ff54f
Ryan Scott <rscott.72.h@gmail.com>
2023-02-07 22:02:24 -0500

    Creates a couple dumb rest endpoints for testing that auto configuration works.

---
+++ src/main/java/com/tau/rest/another_data_rest_service/AnotherDataRestService.java
@@ -0,0 +1,20 @@
+package com.tau.rest.another_data_rest_service;
+
+import com.tau.beans.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class AnotherDataRestService {
+
+    private static final String template = "Goodbye, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/farewell")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
